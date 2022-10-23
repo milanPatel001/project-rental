b561f14bba13318681c1476aa55041d59d651834
milanPatel001 <mp28238@gmail.com>
2022-10-23 17:00:33 -0400

    'init'

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How RentX works</p>
+        <p>News</p>
+        <p>Inv.</p>
+        <p>RentX Plus</p>
+        <p>RentX Lux</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>How RentX works</p>
+        <p>News</p>
+        <p>Inv.</p>
+        <p>RentX Plus</p>
+        <p>RentX Lux</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>How RentX works</p>
+        <p>News</p>
+        <p>Inv.</p>
+        <p>HEHEhEHE</p>
+        <p>RentX Lux</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>How RentX works</p>
+        <p>News</p>
+        <p>Inv.</p>
+        <p>RentX Plus</p>
+        <p>RentX Lux</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
